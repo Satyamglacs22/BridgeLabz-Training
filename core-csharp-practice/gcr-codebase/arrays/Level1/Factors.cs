cfe8f7f2298df69e940449b8289fb42ef6945ca9
SATYAM KUMAR <sk72574401@gmail.com>
2025-12-23 15:26:05 +0530

    [Satyam ] Add . Added Solution of Arrays Level1

---
+++ core-csharp-practice/gcr-codebase/arrays/Level1/Factors.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Arrays
+//{
+//    public class Factors
+//    {
+//        public static void Main()
+//        {
+//            int n = int.Parse(Console.ReadLine());
+//            int count = 0;
+//            for(int i = 1; i <= n; i++)
+//            {
+//                if (n%i == 0)
+//                {
+//                    count++;
+//                }
+//            }
+//            int[] arr = new int[count];
+//            int k = 0;
+//            for(int i = 1; i <= n; i++)
+//            {
+//                if (n % i == 0)
+//                {
+//                    arr[k] = i;
+//                    k++;
+//                }
+//            }
+
+//            for(int i = 0; i < count; i++)
+//            {
+//                Console.WriteLine(arr[i]);
+//            }
+//        }
+        
+//    }
+//}
---
+++ core-csharp-practice/gcr-codebase/arrays/Level1/Table.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+
+//namespace Arrays
+//{
+//    public class Table
+//    {
+//        public static void Main()
+//        {
+//            Console.Write("Enter a number: ");
+//            int n = int.Parse(Console.ReadLine());
+
+//            int[] arr = new int[10];
+
+//            // storing table values
+//            for (int i = 1; i <= 10; i++)
+//            {
+//                arr[i - 1] = n * i;
+//            }
+
+//            // printing table
+//            for (int i = 1; i <= 10; i++)
+//            {
+//                Console.WriteLine($"{n} * {i} = {arr[i - 1]}");
+//            }
+//        }
+//    }
+//}
