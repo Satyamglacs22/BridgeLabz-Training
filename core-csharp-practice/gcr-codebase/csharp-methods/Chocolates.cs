86f1cb51524bb12fc0dd51a95dab07febbeb5b5f
SATYAM KUMAR <sk72574401@gmail.com>
2025-12-24 15:20:19 +0530

    [Satyam] Add .Added Solutions of All Questions of Method Level1

---
+++ core-csharp-practice/gcr-codebase/csharp-methods/Chocolates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method.Level1
+{
+    public class Chocolates
+    {
+        public static void Main()
+        {
+            int choco = int.Parse(Console.ReadLine());
+            int children = int.Parse(Console.ReadLine());
+            int[] arr = new int[2];
+            NumberOfChocolates(choco, children, arr);
+            Console.WriteLine($"Each Children gets {arr[0]} chocolates");
+            Console.WriteLine($"Number of Chocolates left is {arr[1]}");
+        }
+        public static int[] NumberOfChocolates(int choco, int children, int[] arr)
+        {
+            int eachchild = choco / children;
+            int leftchoco = choco % children;
+            arr[0] = eachchild;
+            arr[1] = leftchoco;
+            return arr;
+        }
+    }
+}
---
+++ core-csharp-practice/gcr-codebase/csharp-methods/Temperature.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method.Level1
+{
+    internal class Temperature
+    {
+    }
+}
