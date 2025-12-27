41380ff5d4ea273970f61c658083cf9772443205
SATYAM KUMAR <sk72574401@gmail.com>
2025-12-27 22:25:59 +0530

    Removed unrelated files from review branch

--- core-csharp-practice/gcr-codebase/c#-programming-elements/level1/HandShakes.cs
+++
@@ -1,9 +0,0 @@
-using System;
-public class Handshakes{
-public static void Main(){
-int numberOfStudents;
-numberOfStudents=int.Parse(Console.ReadLine());
-int handshakes=(numberOfStudents*(numberOfStudents-1))/2;
-Console.WriteLine("The maximum number of handshakes is "+handshakes);
-}
-}
--- core-csharp-practice/gcr-codebase/control-flow/Level1/CountDown.cs
+++
@@ -1,6 +0,0 @@
-using System;
-class CountDown{
-static void Main(){
-int n=int.Parse(Console.ReadLine());
-while(n>=1){Console.WriteLine(n);n--;}
-}}
