1863bb7b656a784acc109d67a569ee43e5ebe9f8
SATYAM KUMAR <sk72574401@gmail.com>
2025-12-28 18:02:16 +0530

    [Satyam] Add .Added Extra Built-in Problems

---
+++ core-csharp-practice/gcr-codebase/extra-built-in-practice-problems/level2/DateCompare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class DateCompare
+{
+    public static void Main()
+    {
+        DateTime date1 = DateTime.Parse(Console.ReadLine());
+        DateTime date2 = DateTime.Parse(Console.ReadLine());
+
+        int result = DateTime.Compare(date1, date2);
+
+        if (result < 0)
+            Console.WriteLine("First date is before second date");
+        else if (result > 0)
+            Console.WriteLine("First date is after second date");
+        else
+            Console.WriteLine("Both dates are same");
+    }
+}
---
+++ core-csharp-practice/gcr-codebase/extra-built-in-practice-problems/level2/TimeZones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class TimeZonesDemo
+{
+    public static void Main()
+    {
+        DateTimeOffset utcTime = DateTimeOffset.UtcNow;
+
+        TimeZoneInfo gmt = TimeZoneInfo.Utc;
+        TimeZoneInfo ist = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
+        TimeZoneInfo pst = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+
+        Console.WriteLine("GMT Time: " + TimeZoneInfo.ConvertTime(utcTime, gmt));
+        Console.WriteLine("IST Time: " + TimeZoneInfo.ConvertTime(utcTime, ist));
+        Console.WriteLine("PST Time: " + TimeZoneInfo.ConvertTime(utcTime, pst));
+    }
+}
