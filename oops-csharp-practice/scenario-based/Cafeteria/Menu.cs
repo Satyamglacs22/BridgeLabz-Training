3498c98510981d01151f89155f9533df143c62d6
SATYAM KUMAR <sk72574401@gmail.com>
2026-01-02 16:25:37 +0530

    [Satyam] .Add Added Scenario Based Questions

---
+++ oops-csharp-practice/scenario-based/Cafeteria/Menu.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafeteria
+{
+    public  class Menu
+    {
+        public static void Main()
+        {
+
+            Menu m = new Menu();
+
+
+            string[,] menu = new string[10, 2]
+        {
+            { "Veg Burger", "120" },
+            { "Cheese Pizza", "280" },
+            { "Veg Pasta", "200" },
+            { "French Fries", "150" },
+            { "Veg Sandwich", "130" },
+            { "Paneer Roll", "180" },
+            { "Veg Biryani", "250" },
+            { "Masala Dosa", "140" },
+            { "Cold Coffee", "110" },
+            { "Tea", "40" }
+        };
+
+            bool check = false;
+                while (!check)
+                {
+                Console.WriteLine("Enter 1 For Menu");
+                Console.WriteLine("Enter 2 For Choosing Items");
+                Console.WriteLine("Enter 3 For Termination ");
+
+                int iChoice = int.Parse(Console.ReadLine());
+                    switch (iChoice)
+                    {
+                        case 1:
+                        //Console.WriteLine("Enter 1 For Menu");
+                            m.DisplayMenu(menu);
+                            break;
+
+                        case 2:
+                        //Console.WriteLine("Enter 2 For Choosing Items");
+                        m.GetItemByIndex(menu);
+                            break;
+
+                        case 3:
+                        //Console.WriteLine("Enter 3 For Termination ");
+                            check = true;
+                            break;
+
+
+                    
+                }
+            }
+
+        }
+        public void DisplayMenu(string[,] menu)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{menu[i, 0]}         --------------          {menu[i, 1]}");
+            }
+        }
+        public void GetItemByIndex(string[,] menu)
+
+        {
+            Console.WriteLine("Enter Index to Get Item");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{menu[n - 1, 0]}         --------------         {menu[n - 1, 1]}");
+        }
+
+    }
+}
---
+++ oops-csharp-practice/scenario-based/Diwali-Mela/LuckyDraw.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diwali_Mela
+{
+    public class LuckyDraw
+    {
+        public static void Main()
+        {
+            LuckyDraw obj = new LuckyDraw();
+
+            Console.WriteLine("Enter number of visitors:");
+            int visitors = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= visitors; i++)
+            {
+                Console.WriteLine($"Visitor {i}, enter number:");
+
+                try
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    obj.CheckWin(number);
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input, moving to next visitor.");
+                    continue;
+                }
+            }
+        }
+
+        public void CheckWin(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("Congratulations! You won a gift");
+            }
+            else
+            {
+                Console.WriteLine("Better Luck Next time.");
+            }
+        }
+    }
+}
