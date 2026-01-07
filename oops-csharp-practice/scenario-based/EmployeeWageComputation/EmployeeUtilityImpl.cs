269a3f1e87f6c75f86980a71d4e1f58e1c5b19bd
SATYAM KUMAR <sk72574401@gmail.com>
2026-01-07 16:11:09 +0530

    [Satyam] Add .Added UC-1 Completed

---
+++ oops-csharp-practice/scenario-based/EmployeeWageComputation/EmployeeUtilityImpl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeUtilityImpl : IEmployee
+    {
+        private Random random = new Random();
+        private int wagesPerHour = 20;
+        private int fullDayHour = 8;
+
+        public Employee AddEmployee(int id, string name, double salary)
+        {
+            Employee emp = new Employee();
+            emp.EmployeeId = id;
+            emp.Name = name;
+            emp.EmployeeSalary = salary;
+            return emp;
+        }
+
+        // UC-1 implementation
+        public void CheckAttendance(Employee employee)
+        {
+            int attendance = random.Next(0, 2);
+            employee.Attendance = attendance;
+
+            if (attendance == 1)
+                Console.WriteLine($"Employee {employee.Name} is Present");
+            else
+                Console.WriteLine($"Employee {employee.Name} is Absent");
+        }
+       
+
+        }
+    }
+}
