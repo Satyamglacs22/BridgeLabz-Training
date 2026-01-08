154bb33fed55cf0410543facffb1fcf97a9ecc7d
SATYAM KUMAR <sk72574401@gmail.com>
2026-01-08 12:49:10 +0530

    Folder Deleted

--- core-csharp-practice/gcr-codebase/c#-programming-elements/level1/KmToMiles.cs
+++
@@ -1,9 +0,0 @@
-using System;
-public class KmToMiles{
- public static void  Main(){
-    double km=10.8;
-	double km_1=1.6;
-	Console.WriteLine("The distance"+km +"km in miles is "+ km*km_1);
-	}
-	}
-	
