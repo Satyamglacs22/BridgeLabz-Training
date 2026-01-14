aff834980ad050865e3c07deee47bdc9e53f9a39
SATYAM KUMAR <sk72574401@gmail.com>
2026-01-14 20:54:04 +0530

    [Satyam] Add .Added Algorithm Assignment

---
+++ data-structure-csharp-practice/gcr-codebase/csharp-algorithm/Algorithm/SearchComparison.cs
@@ -0,0 +1,53 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Algorithm
+//{
+//    internal class SearchComparison
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] arr = new int[10000];
+
+//            for (int i = 0; i < arr.Length; i++)
+//            {
+//                arr[i] = i + 1;
+//            }
+
+//            int target = 9000;
+
+//            Console.WriteLine("Linear Search Index: " + LinearSearch(arr, target));
+//            Console.WriteLine("Binary Search Index: " + BinarySearch(arr, target));
+//        }
+
+//        static int LinearSearch(int[] arr, int target)
+//        {
+//            for (int i = 0; i < arr.Length; i++)
+//            {
+//                if (arr[i] == target)
+//                    return i;
+//            }
+//            return -1;
+//        }
+
+//        static int BinarySearch(int[] arr, int target)
+//        {
+//            int left = 0;
+//            int right = arr.Length - 1;
+
+//            while (left <= right)
+//            {
+//                int mid = (left + right) / 2;
+
+//                if (arr[mid] == target)
+//                    return mid;
+//                else if (arr[mid] < target)
+//                    left = mid + 1;
+//                else
+//                    right = mid - 1;
+//            }
+//            return -1;
+//        }
+//    }
+//}
---
+++ data-structure-csharp-practice/gcr-codebase/csharp-algorithm/Algorithm/StringComparison.cs
@@ -0,0 +1,43 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Algorithm
+//{
+//    internal class StringComparison
+//    {
+//        static void Main(string[] args)
+//        {
+//            int n = 10000;
+
+//            // ================= STRING CONCATENATION =================
+//            // Time Complexity: O(N^2)
+//            Console.WriteLine("String Concatenation Started");
+
+//            string normalString = "";
+
+//            for (int i = 0; i < n; i++)
+//            {
+//                normalString = normalString + "A";
+//            }
+
+//            Console.WriteLine("String Concatenation Completed");
+
+//            // ================= STRINGBUILDER CONCATENATION =================
+//            // Time Complexity: O(N)
+//            Console.WriteLine("\nStringBuilder Concatenation Started");
+
+//            StringBuilder sb = new StringBuilder();
+
+//            for (int i = 0; i < n; i++)
+//            {
+//                sb.Append("A");
+//            }
+
+//            Console.WriteLine("StringBuilder Concatenation Completed");
+
+//            Console.WriteLine("\nConclusion:");
+//            Console.WriteLine("StringBuilder is faster and memory efficient than String");
+//        }
+//    }
+//}
