e5d37cc996b95cb15d6ccab75dd967827b764ece
SATYAM KUMAR <sk72574401@gmail.com>
2026-01-14 11:37:34 +0530

    [Satyam] Add .Added UC1 Implemented

---
+++ oops-csharp-practice/scenario-based/AddressBook/Contacts.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    public class Contacts
+    {
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string zip;
+        private string phoneNumber;
+        private string email;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+
+        }
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+            }
+        }
+        public string Zip
+        {
+            get
+            {
+                return zip;
+            }
+            set
+            {
+                zip = value;
+            }
+        }
+        public string PhoneNumber
+        {
+            get
+            {
+                return phoneNumber;
+            }
+            set
+            {
+                phoneNumber = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+
+    }
+}
