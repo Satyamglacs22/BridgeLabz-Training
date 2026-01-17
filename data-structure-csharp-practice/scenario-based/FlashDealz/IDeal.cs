6a6d96f04e9ad664dc3d73f189b45a1e17d936a4
SATYAM KUMAR <sk72574401@gmail.com>
2026-01-17 15:52:16 +0530

    [Satyam] Add .Added Scenario Based Question

---
+++ data-structure-csharp-practice/scenario-based/FlashDealz/IDeal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashDealz
+{
+    internal interface IDeal
+    {
+        void QuickSort(Deal[] deals, int low, int high);
+        void DisplayDeals(Deal[] deals);
+    }
+}
---
+++ data-structure-csharp-practice/scenario-based/FlashDealz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashDealz
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            DealMenu menu = new DealMenu();
+            DealUtility utility = new DealUtility();
+
+            Deal[] deals = menu.TakeDealInput();
+
+            Console.WriteLine("\nBefore Sorting:");
+            utility.DisplayDeals(deals);
+
+            utility.QuickSort(deals, 0, deals.Length - 1);
+
+            Console.WriteLine("\nAfter Sorting (Top Discounts First):");
+            utility.DisplayDeals(deals);
+        }
+    }
+}
